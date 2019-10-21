9a4a2e291765d6a29c1433919930088da7cca374
Nikomof <54985320+Nikomof@users.noreply.github.com>
2019-10-21 17:31:33 +0200

    Test

--- main.ts
+++ main.ts
@@ -2,6 +2,13 @@
  * Groblocks for foodcomputer
  *Set ssid & pw for wifi connect
  */
+ /**
+  * Provides access to basic micro:bit functionality.
+  */
+ //% color=190 weight=100 icon="\uf1ec" block="Basic Blocks"
+ namespace basic {
+
+}
 
 namespace Groblocks {
 }
