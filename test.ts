ea4d9b68b43254fc342c5a75528ddfad5952f5ce
Nikomof <54985320+Nikomof@users.noreply.github.com>
2019-10-22 12:01:36 +0200

    s

--- test.ts
+++ test.ts
@@ -4,12 +4,9 @@ namespace Groblocks {
 
 
 
-  //% blockId="Forbind_til_wifi"
-  //% weight=85 blockGap=8
-  //% parts="Groblock"
-  export function clock(from: number, to: number):void {
-  (from + to);
-  return;
+  //% block
+  export function clock(from: number, to: number) {
+
   }
 
 }
