3ee4cf526d23a59564e88edafc732fd8a30baa40
Nikomof <54985320+Nikomof@users.noreply.github.com>
2019-10-22 11:51:20 +0200

    Update test.ts
    Test

--- test.ts
+++ test.ts
@@ -3,14 +3,13 @@
 namespace Groblocks {
 
 
-//% blockId="Forbind_til_wifi"
-//% weight=85 blockGap=8
-//% parts="Groblock"
-  export function wifiConnect(ssid: string, pw: string,){
 
-  }
-
+  //% blockId="Forbind_til_wifi"
+  //% weight=85 blockGap=8
+  //% parts="Groblock"
   export function clock(from: number, to: number):void {
+  (from + to);
+  return;
   }
 
 }
