e34413def5965947a2a1a400278b614e5afed8b7
Nikomof <54985320+Nikomof@users.noreply.github.com>
2019-10-23 11:14:52 +0200

    Update test.ts

--- test.ts
+++ test.ts
@@ -5,6 +5,10 @@ namespace Test {
 
   export class grobot {
 
+
+    //% blockId="test_grobot_cloak" block="%grobot"
+    //% weight=85 blockGap=8
+    //% parts="neopixel"
     clock(from: number, to: number) {
 
     return (from+to)
