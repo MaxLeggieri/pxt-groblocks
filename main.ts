a1bdbded9c3b4ca18340f4eaf0a432ef77579329
Nikomof <54985320+Nikomof@users.noreply.github.com>
2019-10-25 00:27:06 +0200

    actuators

--- main.ts
+++ main.ts
@@ -1,43 +1,76 @@
-enum testOneTwo {
+/*
+Sensors		         Actuators
+Clock		           Controlled fan
+Humidity		       IR Grow
+Moisture		       UV Grow
+Co2	               5 pumps
+
+
+Sequence #
+*/
+
+
+
+
+enum testEnum {
     //% block="One"
     One,
     //% block="Two"
     Two
+}
+
+enum actuList {
+  //% block="Heater"
+  heater,
+  //% block="fan"
+  fan,
+  //% block="Pump"
+  pump,
+
 }
 /**
  * Turtle graphics blocks
  */
 //% weight=100 color=#0f9c11 icon="\uf188"
 namespace groblocks {
-    let _x: number;
-    let _y: number;
+    //let _x: number;
+    //let _y: number;
     //let _direction: number; // 0 right, 1 down, 2 left, 3 top
     //let _brightness: number;
     //let _penMode: TurtlePenMode = TurtlePenMode.Up;
     //let _img: Image;
-    let _delay = 250;
-
-
-    /**
-     * times hours
-     * @param timer number of hours, eg: 1
-     */
-    //% blockId=turtleForward block="count %timer|hours"
-    //% weight=99 blockGap=8
-    export function clock(timer: number): void {
-
-        }
+    //let _delay = 250;
 
     /**
     * times hours
     * @param testString string
     */
-    //% blockId=turtleForward2 block="Show %testString|as string"
+    //% blockId=testSerialPrint block="Test Serial print: %testString| "
     //% weight=99 blockGap=8
-    export function testSerial(testString: string):void{
+    export function testSerialPrint(testString: string):void{
       basic.showString(testString);
-  }
+      serial.serialWrite(teststring);
+    }
 
+    /**
+     * times hours
+     * @param mode enum, actuators eg fan or heater
+     */
+    //% blockId=testActu="Choose an actuator %mode"
+    //% weight=99 blockGap=8
+    export function testActu(mode: actuList): void {
+    basic.showString(mode)
+    }
+
+
+  /**
+  * times hours
+  */
+  //% blockId=testSensor block="Show number"
+  //% weight=99 blockGap=8
+  export function testSensor():void {
+
+  }
 
 
 }
