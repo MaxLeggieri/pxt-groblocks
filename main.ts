7d8022fdb26f1723d9f99be9115c60b48a4f88d5
Nikomof <54985320+Nikomof@users.noreply.github.com>
2019-10-25 11:33:36 +0200

    update

--- main.ts
+++ main.ts
@@ -4,14 +4,8 @@ Clock		           Controlled fan
 Humidity		       IR Grow
 Moisture		       UV Grow
 Co2	               5 pumps
-
-
 Sequence #
 */
-
-
-
-
 enum testEnum {
     One,
     Two,
@@ -27,16 +21,10 @@ enum actuList {
 }
 
 
-/**
- * Grobot graphics block
- */
+
+// groblocks graphics
 //% weight=100 color=#0f9c11 icon="\uf188"
 namespace groblocks {
-    //let _x: number;
-    //let _y: number;
-
-
-
 
 
     /**
@@ -51,29 +39,23 @@ namespace groblocks {
     }
 
     /**
-     * actuator
-     */
+    * Test loaded from comment
+    */
     //% blockId=testActu block="Choose an actuator %mode"
     //% weight=99 blockGap=8
     export function testActu(mode: actuList) : void {
-    serial.writeString()
+      serial.writeString()
     }
 
 
-
-  //% blockId=testShadow  block="Shadow test|log %msg"
+  //% blockId=testShadow  block="Shadow test %msg"
   //% text.shadowOptions.toString=true
   export function testShadow(text: string): void {
       serial.writeString(text + "\r\n");
   }
-  /**
-  * times hours
-  */
-  //% blockId=testSensor block="Hummidity"
-  //% weight=99 blockGap=8
-  //export function testSensor() : void {
 
-//  }
+
+
 
 
 }
